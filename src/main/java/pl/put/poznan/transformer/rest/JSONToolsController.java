06a11bb79ec21f94571f006d9d32e4322c12fdb3
wachuuu <gab.wachowski15@gmail.com>
2021-12-14 11:22:30 +0100

    project architecture frame

---
+++ src/main/java/pl/put/poznan/transformer/rest/JSONToolsController.java
@@ -0,0 +1,41 @@
+package pl.put.poznan.transformer.rest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.logic.TextTransformer;
+
+import java.util.Arrays;
+
+
+@RestController
+@RequestMapping("/{text}")
+public class JSONToolsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(JSONToolsController.class);
+
+    @GetMapping(produces = "application/json")
+    public String get(@PathVariable String text,
+                      @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(Arrays.toString(transforms));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        TextTransformer transformer = new TextTransformer(transforms);
+        return transformer.transform(text);
+    }
+
+    @PostMapping(produces = "application/json")
+    public String post(@PathVariable String text,
+                       @RequestBody String[] transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(Arrays.toString(transforms));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        TextTransformer transformer = new TextTransformer(transforms);
+        return transformer.transform(text);
+    }
+}
