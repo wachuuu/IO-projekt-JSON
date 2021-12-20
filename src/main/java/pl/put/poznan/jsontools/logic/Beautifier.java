de9e029d110f3f77bc8dd639bb3059eddd37eddd
stachu1190 <stachu1190@gmail.com>
2021-12-20 16:27:18 +0100

    Implemented Beautifier

--- src/main/java/pl/put/poznan/jsontools/logic/Beautifier.java
+++ src/main/java/pl/put/poznan/jsontools/logic/Beautifier.java
@@ -1,11 +1,26 @@
 package pl.put.poznan.jsontools.logic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
 public class Beautifier extends JSONModifier {
-    public Beautifier(String body) {
+    public Beautifier(Map<String, Object> body) {
         super(body);
     }
 
     public String modify() {
-        return body;
+        //System.out.println(body.toString());
+        ObjectMapper mapper = new ObjectMapper();
+
+        String beautified = null;
+        try {
+            beautified = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        //String beautified = "aaa";
+        return beautified;
     }
 }
--- src/main/java/pl/put/poznan/jsontools/logic/Minifier.java
+++ src/main/java/pl/put/poznan/jsontools/logic/Minifier.java
@@ -1,11 +1,13 @@
 package pl.put.poznan.jsontools.logic;
 
+import java.util.Map;
+
 public class Minifier extends JSONModifier{
-    public Minifier(String body) {
+    public Minifier(Map<String, Object> body) {
         super(body);
     }
 
     public String modify() {
-        return body;
+        return "sample";
     }
 }
