acd3abdf6f51bcac6417b14c2c44f9cd2c54c9d8
pvlv11 <igorpawlowski11@gmail.com>
2021-12-20 20:30:15 +0100

    Implemented minification

--- src/main/java/pl/put/poznan/jsontools/logic/Minifier.java
+++ src/main/java/pl/put/poznan/jsontools/logic/Minifier.java
@@ -1,13 +1,27 @@
 package pl.put.poznan.jsontools.logic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.Map;
 
-public class Minifier extends JSONModifier{
+public class Minifier extends JSONModifier {
     public Minifier(Map<String, Object> body) {
         super(body);
     }
 
     public String modify() {
-        return "sample";
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String beautified = null;
+
+        try {
+            beautified = mapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return beautified;
     }
 }
